c10c74e6ad86436240a6ea1ef2b3e88a6ce423ef
raed-abodayy <raedabodyu@gmail.com>
2025-03-26 16:58:50 +0200

    Add files via upload

---
+++ Rotate Array/Rotate Array.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int arrlen = nums.Length;
+        k%=arrlen;
+        Reverse(nums, 0, arrlen - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, arrlen - 1);
+    }
+
+    private void Reverse(int[] nums, int front, int back)
+    {
+        while (front < back)
+        {
+            (nums[front], nums[back]) = (nums[back], nums[front]);
+            front++;
+            back--;
+        }
+    }
+}
